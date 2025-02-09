d0435429d71f2c8c402edbdcb4b7f418634ef62f
十玖八柒(ahzoo) <1359256243@qq.com>
2025-02-09 17:58:01 +0800

    feat: 增加数据库文件

---
+++ z2blog_lite/1.2.1/blog_z2.sql
@@ -0,0 +1,209 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : mysql80
+ Source Server Type    : MySQL
+ Source Server Version : 80031
+ Source Host           : localhost:3307
+ Source Schema         : blog_z2
+
+ Target Server Type    : MySQL
+ Target Server Version : 80031
+ File Encoding         : 65001
+ Author                : Ahzoo
+ Github                : https://github.com/ooahz
+ Date: 21/12/2024 17:05:38
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for b_access
+-- ----------------------------
+DROP TABLE IF EXISTS `b_access`;
+CREATE TABLE `b_access`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `uv` bigint NULL DEFAULT NULL,
+  `pv` bigint NULL DEFAULT NULL,
+  `date` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '数据统计的时间',
+  `updated_date` datetime NOT NULL COMMENT '数据写入时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 64 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for b_access_article
+-- ----------------------------
+DROP TABLE IF EXISTS `b_access_article`;
+CREATE TABLE `b_access_article`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `article_id` bigint NULL DEFAULT NULL,
+  `pv` bigint NULL DEFAULT NULL,
+  `date` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '数据统计的时间',
+  `updated_date` datetime NOT NULL COMMENT '数据写入时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 75 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for b_article
+-- ----------------------------
+DROP TABLE IF EXISTS `b_article`;
+CREATE TABLE `b_article`  (
+  `id` bigint NOT NULL,
+  `path` varchar(24) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '文章路径',
+  `title` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '文章标题',
+  `description` varchar(520) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '文章概述',
+  `thumbnail` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '文章预览图',
+  `style` varchar(520) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '文章页背景样式',
+  `created_date` datetime NOT NULL,
+  `updated_date` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `type` tinyint(1) NOT NULL DEFAULT 1 COMMENT '类型（1：原创，2：转载）',
+  `weight` tinyint NOT NULL DEFAULT 1 COMMENT '权重',
+  `status` tinyint(1) NOT NULL DEFAULT 1 COMMENT '状态（1：正常，2：隐藏，3：草稿）',
+  `deprecated` tinyint(1) NOT NULL DEFAULT 0 COMMENT '废弃（1：废弃，0：正常）',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `idx_b_article_p`(`path`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin COMMENT = '文章表：\r\n作者：十玖八柒（Ahzoo）\r\nGithub：https://github.com/ooahz' ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for b_article_column
+-- ----------------------------
+DROP TABLE IF EXISTS `b_article_column`;
+CREATE TABLE `b_article_column`  (
+  `column_id` bigint NOT NULL,
+  `article_id` bigint NOT NULL,
+  INDEX `idx_b_article_ci`(`column_id`) USING BTREE,
+  INDEX `idx_b_article_ai`(`article_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for b_article_content
+-- ----------------------------
+DROP TABLE IF EXISTS `b_article_content`;
+CREATE TABLE `b_article_content`  (
+  `article_id` bigint NOT NULL,
+  `content_md` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL COMMENT 'markdown原文',
+  `content_html` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL COMMENT 'html原文',
+  PRIMARY KEY (`article_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for b_category
+-- ----------------------------
+DROP TABLE IF EXISTS `b_category`;
+CREATE TABLE `b_category`  (
+  `id` bigint NOT NULL,
+  `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '分类名',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for b_category_column
+-- ----------------------------
+DROP TABLE IF EXISTS `b_category_column`;
+CREATE TABLE `b_category_column`  (
+  `category_id` bigint NOT NULL,
+  `column_id` bigint NOT NULL,
+  INDEX `idx_b_article_cai`(`category_id`) USING BTREE,
+  INDEX `idx_b_article_coi`(`column_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for b_column
+-- ----------------------------
+DROP TABLE IF EXISTS `b_column`;
+CREATE TABLE `b_column`  (
+  `id` bigint NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '专栏名',
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '专栏简介',
+  `thumbnail` varchar(520) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '专栏缩略图',
+  `style` varchar(520) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL COMMENT '专栏背景样式',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for b_comment
+-- ----------------------------
+DROP TABLE IF EXISTS `b_comment`;
+CREATE TABLE `b_comment`  (
+  `id` bigint NOT NULL,
+  `article_id` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '文章标识',
+  `website` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '站点标识',
+  `parent_id` bigint NULL DEFAULT 0 COMMENT '父评论',
+  `user_avatar` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `user_name` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `user_website` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `user_email` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `reply_name` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '回复用户',
+  `content` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `ip` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `area` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '归属地',
+  `user_agent` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '客户端',
+  `created_date` datetime NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1 COMMENT '状态(0:不可见,1:正常)',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for b_friend
+-- ----------------------------
+DROP TABLE IF EXISTS `b_friend`;
+CREATE TABLE `b_friend`  (
+  `id` bigint NOT NULL,
+  `old_website` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `website` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `weight` int NULL DEFAULT NULL COMMENT '权重',
+  `type` tinyint(1) NOT NULL DEFAULT 1 COMMENT '类型(1:默认,2:技术,3:生活）',
+  `status` tinyint(1) NOT NULL DEFAULT 1 COMMENT '状态(0:不可见,1:正常,2审核中,3待更新)',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for sys_log
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_log`;
+CREATE TABLE `sys_log`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NULL DEFAULT NULL,
+  `method_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '方法名',
+  `request_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '请求方式',
+  `request_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '请求源地址',
+  `request_ip` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '请求源ip',
+  `param` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '请求参数',
+  `content` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `created_time` datetime NULL DEFAULT NULL COMMENT '操作时间',
+  `updated_time` datetime NOT NULL COMMENT '写入时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for sys_role
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_role`;
+CREATE TABLE `sys_role`  (
+  `id` bigint NOT NULL,
+  `user_id` bigint NOT NULL,
+  `role_code` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for sys_user
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user`;
+CREATE TABLE `sys_user`  (
+  `id` bigint NOT NULL,
+  `email` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `salt` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL,
+  `status` tinyint NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `idx_sys_user_e`(`email`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = DYNAMIC;
+
+SET FOREIGN_KEY_CHECKS = 1;
